572215ca28992a1508c58798643fad72a257ca32
Nikhilbabuthachattil <Nikhilbabuthachattil@gmail.com>
2024-05-10 18:25:05 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {  HashRouter} from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename='/'>
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
